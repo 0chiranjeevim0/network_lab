f0e3d120f591d7cf2a17b49b625dcca8ba351492
CHIRANJEEVI <chiranjeevi2004@gmail.com>
2023-02-08 20:43:45 +0530

    status:UDP SOCKETS

--- udp_sockets/clientSocket.java
+++ udp_sockets/clientSocket.java
@@ -1,32 +1,42 @@
 import java.io.*;
-import java.net.*;
 import java.util.*;
-class clientSocket
-{
-            public static void main(String args[])
-            {
-            try
-            {
-                        DatagramSocket client=new DatagramSocket();
-                        InetAddress addr=InetAddress.getByName("127.0.0.1");
+import java.net.*;
 
-                        byte[] sendbyte=new byte[1024];
-                        byte[] receivebyte=new byte[1024];
-                        BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
-                        System.out.println("Enter the DOMAIN NAME or IP adress:");
-                        String str=in.readLine();
-                        sendbyte=str.getBytes();
-                        DatagramPacket sender=new DatagramPacket(sendbyte,sendbyte.length,addr,1309);
-                        client.send(sender);
-                        DatagramPacket receiver=new DatagramPacket(receivebyte,receivebyte.length);
-                        client.receive(receiver);
-                        String s=new String(receiver.getData());
-                        System.out.println("IP address or DOMAIN NAME: "+s.trim());   
-                        client.close();
-            }
-            catch(Exception e)
-            {
-                        System.out.println(e);
-            }
-            }
+
+
+
+public class clientSocket{
+      public static void main(String args[]){
+           try{
+           		DatagramSocket client = new DatagramSocket();
+            	InetAddress add = InetAddress.getByName("127.0.0.1");
+            	BufferedReader s = new BufferedReader(new InputStreamReader(System.in));
+
+            
+            	byte[] sendbyte = new byte[1024];
+            	byte[] recievebyte = new byte[1024];
+
+            	System.out.println("Enter DOMAIN NAME");
+            	String str = s.readLine();
+            	sendbyte = str.getBytes();
+
+            	DatagramPacket sender = new DatagramPacket(sendbyte,sendbyte.length,add,1309);
+            	DatagramPacket reciever = new DatagramPacket(recievebyte,recievebyte.length);
+            	client.send(sender);
+
+
+            	client.receive(reciever);
+
+            	String ip = new String(reciever.getData());
+            	System.out.println("IP ADDRESS:"+ip.trim());
+
+            	client.close();
+
+           }catch(Exception e){
+                  System.out.println(e.getMessage());
+           }
+
+
+
+      }
 }
--- udp_sockets/serverSocket.java
+++ udp_sockets/serverSocket.java
@@ -14,24 +14,19 @@ class serverSocket
                                     byte[] receivebyte=new byte[1024];
                                     DatagramPacket receiver=new DatagramPacket(receivebyte,receivebyte.length);
                                     server.receive(receiver);
+
                                     String str=new String(receiver.getData());
                                     String s=str.trim();
+
                                     //System.out.println(s);
                                     InetAddress addr=receiver.getAddress();
                                     int port=receiver.getPort();
+
                                     String ip[]={"165.165.80.80","165.165.79.1"};
                                     String name[]={"www.aptitudeguru.com","www.downloadcyclone.blogspot.com"};
                                     for(int i=0;i<ip.length;i++)
                                     {
-                                                if(s.equals(ip[i]))
-                                                {
-                                                            sendbyte=name[i].getBytes();
-                                                            DatagramPacket sender=new DatagramPacket(sendbyte,sendbyte.length,addr,port);
-                                                            server.send(sender);
-                                                            break;
-                                                }
-                                                else if(s.equals(name[i]))
-                                                {
+                                            	if(s.equals(name[i])){
                                                             sendbyte=ip[i].getBytes();
                                                             DatagramPacket sender=new DatagramPacket(sendbyte,sendbyte.length,addr,port);
                                                             server.send(sender);
